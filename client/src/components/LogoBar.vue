b165eb1a34d420442f1be236f05eec9c612c9dfb
Mina Mansour <mansourmina10@gmail.com>
2021-09-24 14:01:42 +0200

    first commit

---
+++ client/src/components/LogoBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+
+     <v-app-bar
+      app
+      color="dark"
+      dark
+      height="200"
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="m-10 "
+          contain
+          color="dark"
+          src="logo.png"
+          transition="scale-transition"
+          width="280"
+        />
+      </div>
+     </v-app-bar>
+
+      <v-spacer></v-spacer>
+  
+
+  </v-container>
+</template>
+
+
+
---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container>
+  <LogoBar />
+  <CarCards/>
+  </v-container>
+</template>
+
+<script>
+  import LogoBar from '../components/LogoBar.vue'
+  import CarCards from '../components/CarCards.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      LogoBar,
+      CarCards
+    },
+  }
+</script>
